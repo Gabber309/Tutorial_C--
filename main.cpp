85304254cef52baa18d2177a82a74d4973f15d8f
TEXflip <michele.tessari12@gmail.com>
2023-04-17 21:45:45 +0200

    Capendo cose

---
+++ main.cpp
@@ -0,0 +1,2 @@
+//add_executable(app) //questo è il nome del file eseguibile
+//target_sources(app PRIVATE "main.cpp") //questo è il nome del file sorgente, private significa che non è visibile all'esterno
